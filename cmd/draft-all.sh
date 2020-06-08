ade166b4398555c0da5f262f447dc156a4ae395c
Luca Sepe <luca.sepe@gmail.com>
2020-06-08 14:18:32 +0200

    [issue #1] common provider for one or more nodes
    * groups components with the same `provider` attribute (see examples)
    * add the new attribute `impl` to all components (see examples)
    * change microservice shape to 'octagon'
    * layout README file

---
+++ cmd/draft-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SRC_DIR=../examples
+DPI=120
+EXE=./dist/draft_linux_amd64/draft
+
+## declare an array of files
+declare -a arr=("$SRC_DIR/client.yml" 
+                "$SRC_DIR/service.yml"
+                "$SRC_DIR/broker.yml"
+                "$SRC_DIR/gateway.yml"
+                "$SRC_DIR/queue.yml"
+                "$SRC_DIR/function.yml"
+                "$SRC_DIR/database.yml"
+                "$SRC_DIR/storage.yml"
+                "$SRC_DIR/message-bus-pattern.yml"
+                "$SRC_DIR/aws-cognito-custom-auth-flow.yml"
+                "$SRC_DIR/s3-upload-presigned-url.yml"
+                "$SRC_DIR/backend-for-frontend.yml" )
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   # grab the filename without extension
+   filename=$(basename -- "$i")
+   # run draft...run!
+   "$EXE" "$i" | dot -Tpng -Gdpi=$DPI > "$SRC_DIR/${filename%.*}.png"
+done
